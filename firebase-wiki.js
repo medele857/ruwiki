0fb6545d93f333ebee102797c0a272fa7dc482ca
mdl <kirilleroshenko1711@gmail.com>
2026-06-27 10:34:46 +0400

    Update firebase-wiki.js

--- firebase-wiki.js
+++ firebase-wiki.js
@@ -1,21 +1,17 @@
-/**
- * firebase-wiki.js  v2
- * Подключай на КАЖДУЮ страницу (включая main.html) после script.js
- */
+
 (function () {
   'use strict';
 
-  var FIREBASE_CFG = {
-    apiKey:            "AIzaSyAID9ivHW94RB6eMB9qG4kVK7QfK2iStgc",
-    authDomain:        "ruwiki-f1ecb.firebaseapp.com",
-    projectId:         "ruwiki-f1ecb",
-    storageBucket:     "ruwiki-f1ecb.firebasestorage.app",
-    messagingSenderId: "645896740533",
-    appId:             "1:645896740533:web:169a140bb01857fec0a8fc",
-    databaseURL:       "https://ruwiki-f1ecb-default-rtdb.europe-west1.firebasedatabase.app"
-  };
+ var FIREBASE_CFG = {
+  apiKey:            "AIzaSyAID9ivHW94RB6eMB9qG4kVK7QfK2iStgc",
+  authDomain:        "ruwiki-f1ecb.firebaseapp.com",
+  projectId:         "ruwiki-f1ecb",
+  storageBucket:     "ruwiki-f1ecb.firebasestorage.app",
+  messagingSenderId: "645896740533",
+  appId:             "1:645896740533:web:169a140bb01857fec0a8fc",
+  databaseURL:       "https://ruwiki-f1ecb-default-rtdb.firebaseio.com"
+};
 
-  // ── Все записи вики (для топ-3 на главной) ──
   var WIKI_ENTRIES = {
     'studio-cubikarti':  { name:'Куби карты',   logo:'cubikarti.png',        type:'Студия',       url:'studio-cubikarti.html' },
     'studio-qwer':       { name:'Qwer Team',     logo:'banka.png',            type:'Студия',       url:'studio-qwer.html' },
